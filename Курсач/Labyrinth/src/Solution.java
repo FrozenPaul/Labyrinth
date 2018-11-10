b7aa06c46e627835dee1e62c25ab48a4e66f2535
FrozenPaul <44210828+FrozenPaul@users.noreply.github.com>
2018-11-11 02:45:52 +0300

    Delete Solution.java

--- Курсач/Labyrinth/src/Solution.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        Labyrinth a = new Labyrinth("file.txt");
-        a.findAWayOut();
-    }
-}
